5672f9d84effd2108bc5422f32219cbcf8a54c85
EmirGaziKopar <1190505016@ogrenci.kirklareli.edu.tr>
2023-05-19 07:20:49 +0300

    1.7

--- SpineTest/Assets/CharacterMovement.cs
+++ SpineTest/Assets/CharacterMovement.cs
@@ -24,7 +24,7 @@ public class CharacterMovement : MonoBehaviour
 
     
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         //ikinci seçenek rigidbody2D.velocity += new vector2() yaparak belli saniye aralýðýnda çalýþtýrmak. Hem zýplama hem yuvarlanma yapmamalý.
         //Burada karaktere 0.5f saniye boyunca vektör toplamasý uyguluyoruz.
@@ -57,9 +57,10 @@ public class CharacterMovement : MonoBehaviour
             */
             if (rollTime < 0.5f)
             {
-                rigidbody2D.velocity += new Vector2(rolSpeed*Time.deltaTime , 0); //Burada time.deltaTime kullanmaya gerek vardýr çünkü kare baþýna anlýk artan deðerlerin toplamýný hesaplýyoruz.  Bu çaðýrýldýðý miktar kadar toplanan bir vektörü simgeler.           
-                rollTime += Time.deltaTime;
-                
+                rigidbody2D.velocity += new Vector2(rolSpeed, 0);
+               
+                rollTime += Time.fixedDeltaTime;
+
             }
 
             else
@@ -70,16 +71,16 @@ public class CharacterMovement : MonoBehaviour
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position += new Vector3((-speed / 50) * Time.deltaTime, 0, 0);
+            transform.position += new Vector3(-speed / 50, 0, 0);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position += new Vector3((speed / 50) * Time.deltaTime, 0, 0);
+            transform.position += new Vector3(speed / 50, 0, 0);
         }
         //rigidbody2D.AddForce(new Vector2(1,1));
         if (CharacterAnimController.leftMove)
         {
-            transform.position += new Vector3((-speed/50) * Time.deltaTime, 0 , 0);
+            transform.position += new Vector3(-speed/50, 0 , 0);
             /*if(counter == 0) // counter == 0 ise karakter yer ile temas etmiþtir ve yerdedir bu nedenle zemine doðru vektör uygulanmasýna gerek yoktur.
             {
                 rigidbody2D.velocity = new Vector2(-speed * 1, 0);
@@ -95,7 +96,7 @@ public class CharacterMovement : MonoBehaviour
         }
         if (CharacterAnimController.rightMove)
         {
-            transform.position += new Vector3((speed / 50) * Time.deltaTime, 0, 0);
+            transform.position += new Vector3(speed / 50, 0, 0);
             /*
             if (counter == 0)
             {
@@ -116,7 +117,7 @@ public class CharacterMovement : MonoBehaviour
             if (counter < 1)
             {
                 counter++;
-                rigidbody2D.velocity = new Vector2(0, jumpParametr * 1);  //Burada time.deltaTime kullanmaya gerek yoktur çünkü kare baþýna anlýk artan deðerlerin toplamýný hesaplamýyoruz. ör transform.position += ....
+                rigidbody2D.velocity = new Vector2(0, jumpParametr * 1);
             }
             
         }
